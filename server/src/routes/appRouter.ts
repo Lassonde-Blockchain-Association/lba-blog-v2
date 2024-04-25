ec82f270880c3d33a70ac541986e7dc9999ad901
theo-thanhlam <theo.thanhlam@gmail.com>
2024-04-24 21:54:46 -0400

    fix: add author router

--- server/src/routes/appRouter.ts
+++ server/src/routes/appRouter.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc"
 import { authRouter } from "./auth.route"
+import { authorRouter } from "./author.route"
 import { blogRouter } from "./blog.route"
 
 export const appRouter = router({
     blog: blogRouter,
     auth: authRouter,
+    author: authorRouter,
 })
